a6a0adcd39fbbf76d2f730efa032454d800f2e3f
LexLuter <gae_cbr@mail.ru>
2023-02-15 00:32:19 +0700

    modify Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 1){
+    int i = 2;
+    while (i <= N){
+        Console.WriteLine(i);
+        i = i+2;
+    }
+}
+else Console.WriteLine("число " + N + " меньше 2-х" );
